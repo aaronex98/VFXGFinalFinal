3cd134ae74c9f812f075fad0d2ca91df13c82441
aaronex98 <adiaz_ramos@hotmail.com>
2019-12-17 18:54:29 +0100

    cosas

--- VFXGrupal/Assets/Scripts/PlayerMovement.cs
+++ VFXGrupal/Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
+    private Camera cam;
+    public static Ray ray;
+    private RaycastHit hit;
+    public float rotSpeed;
+ 
      void Start()
     {
-        
+        cam = Camera.main;
     }
 
      void FixedUpdate()
@@ -36,8 +41,30 @@ public class PlayerMovement : MonoBehaviour
             this.transform.position = Vector3.Lerp(this.transform.position, this.transform.position - this.transform.forward, Time.deltaTime * speed);
 
         }
+      
+        if (Input.mousePosition.x < Screen.width * 1/3)
+        {
 
+            cam.transform.Rotate(-cam.transform.up * Time.deltaTime * rotSpeed);
 
+        }
+        else if (Input.mousePosition.x > Screen.width * 2 / 3)
+        {
 
+            cam.transform.Rotate( cam.transform.up * Time.deltaTime * rotSpeed);
+
+        }
+        if (Input.mousePosition.y < Screen.width * 1 / 3)
+        {
+
+            cam.transform.Rotate(-cam.transform.x * Time.deltaTime * rotSpeed);
+
+        }
+        else if (Input.mousePosition.y > Screen.width * 2 / 3)
+        {
+
+            cam.transform.Rotate(cam.transform.right * Time.deltaTime * rotSpeed);
+
+        }
     }
 }
