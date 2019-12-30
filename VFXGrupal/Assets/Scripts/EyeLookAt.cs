a38b6ba32c3cb4e6cbc7ed9d246686926514ccb0
aaronex98 <adiaz_ramos@hotmail.com>
2019-12-30 20:29:39 +0300

    miniojos y nebulosa

---
+++ VFXGrupal/Assets/Scripts/EyeLookAt.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeLookAt : MonoBehaviour
+{
+    public GameObject cam;
+    public float timer = 2;
+     void Start()
+    {
+        StartCoroutine(TimerToLook());
+    }
+
+     void Update()
+    {
+        
+    }
+    public IEnumerator TimerToLook()
+    {
+
+
+
+
+        yield return new WaitForSeconds(timer);
+        transform.LookAt(cam.transform);
+        yield return StartCoroutine(TimerToLook());
+    }
+}
